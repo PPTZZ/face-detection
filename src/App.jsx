d7e454e8b01613b11a5234acae9947301d6d6ff0
Alex <poputa.alex@gmail.com>
2023-10-10 22:25:06 +0300

    started the app using vite and pnpm

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import {Component} from 'react'
+import Navigation from './assets/Navigation/Navigation'
+import './App.css'
+
+class App extends Component{
+  render(){
+    return(
+      <>
+        <div className='App'>
+          <Navigation/>
+          {/* {<Logo/>
+          <ImageLinkForm/>
+          <FaceRecognition/>} */}
+        </div>
+      </>
+    )
+  }
+}
+
+export default App
---
+++ src/assets/Navigation/Navigation.jsx
@@ -0,0 +1,14 @@
+
+const Navigation = ()=>{
+    return(
+        <>
+          <nav style={{display:'flex',justifyContent:'flex-end'}}>
+            <p className='pa3 underline'>
+              Sign Out
+            </p>
+          </nav>
+        </>
+    )
+}
+
+export default Navigation;
