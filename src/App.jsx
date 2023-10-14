ac241a424584b3e5c4f7d91062c64338afd29a2d
Alex <poputa.alex@gmail.com>
2023-10-14 14:11:05 +0300

    added particles

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import {Component} from 'react'
+import TParticles from './assets/TSParticles/TSParticles'
 import Navigation from './assets/Navigation/Navigation'
 import Logo from './assets/Logo/Logo'
 import ImageLinkForm from './assets/ImageLinkForm/ImageLinkForm'
+import Rank from './assets/Rank/Rank'
 import './App.css'
+
 
 class App extends Component{
   render(){
     return(
       <>
         <div className='App'>
+          <TParticles/>
           <Navigation/>
           <Logo/>
+          <Rank/>
           <ImageLinkForm/>
           {/* {<FaceRecognition/>} */}
         </div>
---
+++ src/assets/Rank/Rank.jsx
@@ -0,0 +1,14 @@
+const Rank = ()=>{
+    const user = ''
+    const rank=''
+    return(
+        <>
+          <div className="white f2 tc">
+                {`${user} your current rank is ${rank}`}
+          </div>  
+        </>
+    )
+}
+
+export default Rank;
+
